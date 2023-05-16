0ba486ece8bcef0a275472e69838581c88a98b65
sergei1212331 <90478003+sergei1212331@users.noreply.github.com>
2023-05-16 16:14:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше слово: ");
+        string word1 = Console.ReadLine();
+
+        Console.Write("Введіть друге слово: ");
+        string word2 = Console.ReadLine();
+
+        string commonLetters = new string(word1.Intersect(word2).Distinct().ToArray());
+
+        Console.Write("Літери, які є в обох словах: ");
+        for (int i = 0; i < commonLetters.Length; i++)
+        {
+            Console.Write(commonLetters[i]);
+            if (i < commonLetters.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.ReadKey();
+    }
+}
